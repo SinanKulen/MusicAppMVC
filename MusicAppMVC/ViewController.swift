d2cf3c89b965338991ad3b5bbc7a09b727193312
Sinan Kulen <sinan.kulen@gmail.com>
2022-03-01 15:03:29 +0300

    Initial Commit

---
+++ MusicAppMVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicAppMVC
+//
+//  Created by Sinan Kulen on 1.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
